d5c1bbb044f8a2cbde2c64d5f95d366c79b4ccf0
AMeyer33 <andrew.meyer@stu.daviess.kyschools.us>
2026-03-12 19:26:33 +0000

    Commit

--- AInsertionSort.java
+++ AInsertionSort.java
@@ -4,6 +4,17 @@ public class AInsertionSort
     public static void main(String[] args)
     {
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
-
+        for(int x = 1; x < myNumbers.length; x++)
+        {
+            int current = myNumbers[x];
+            int j = myNumbers[x] - 1;
+            while(j >= 0 && myNumbers[j] > current)
+            {
+                myNumbers[j+1] = myNumbers[j];
+                j--;
+            }
+            myNumbers[j+1] = current;
+        }
+        System.out.println(myNumbers);
     }
 }
--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -10,25 +10,31 @@ public class ALInsertionSort
     	boolean inserted=false;
 
 		//Add the first number to the array list
-
+		sorted.add(myNumbers[0]);
 		//Loop once for all of the remaining numbers in the unsorted list
-
+		for(int x = 1; x < myNumbers.length; x++)
+		{
 			//Check with each of the numbers in the sorted list
-
+			for(Integer y: sorted)
 				//If this number is less than one in the sorted list,
 				//insert it there
-
+			if(myNumbers[x] < sorted[y])
+			{
+				sorted.add(myNumbers[x]);
+			}
 			//If it was not inserted, stick it on the end.
-
+			if(myNumbers[x] > sorted[y])
+			{
+				sorted.add(myNumbers[x]);
+			}
 
 		//End Loop for unsorted list
-
-		System.out.println(sorted);
+		}
+		
 
 		//Move the data back to the array
-
 		//Print the contents of the array
-
+		System.out.println(sorted);
     }
 
 
