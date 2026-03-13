db723516f21639173dee016fdcdfa18f8377208e
AMeyer33 <andrew.meyer@stu.daviess.kyschools.us>
2026-03-13 01:02:02 +0000

    Commit

--- AInsertionSort.java
+++ AInsertionSort.java
@@ -1,20 +1,28 @@
+import java.util.Arrays;
+
 public class AInsertionSort
 {
 
     public static void main(String[] args)
     {
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
+        int tempNum = 0;
+
         for(int x = 1; x < myNumbers.length; x++)
         {
-            int current = myNumbers[x];
-            int j = myNumbers[x] - 1;
-            while(j >= 0 && myNumbers[j] > current)
+            for(int y = 0; y < x; y++)
             {
-                myNumbers[j+1] = myNumbers[j];
-                j--;
+                if(myNumbers[x] < myNumbers[y])
+                {
+                    tempNum = myNumbers[x];
+                    for(int z = x; z > y; z--)
+                    {
+                        myNumbers[z] = myNumbers[z-1];
+                    }
+                    myNumbers[y] = tempNum;
+                }
             }
-            myNumbers[j+1] = current;
         }
-        System.out.println(myNumbers);
+        System.out.println(Arrays.toString(myNumbers));
     }
 }
--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ALInsertionSort
 {
@@ -15,26 +16,32 @@ public class ALInsertionSort
 		for(int x = 1; x < myNumbers.length; x++)
 		{
 			//Check with each of the numbers in the sorted list
-			for(Integer y: sorted)
+			for(int y = 0; y < sorted.size() && !inserted; y++)
+			{
 				//If this number is less than one in the sorted list,
 				//insert it there
-			if(myNumbers[x] < sorted[y])
-			{
-				sorted.add(myNumbers[x]);
-			}
-			//If it was not inserted, stick it on the end.
-			if(myNumbers[x] > sorted[y])
-			{
-				sorted.add(myNumbers[x]);
+				if(myNumbers[x] < sorted.get(y))
+				{
+					sorted.add(y, myNumbers[x]);
+					inserted = true;
+				}
 			}
 
+			//If it was not inserted, stick it on the end.
+			if(!inserted)
+			{
+				sorted.add(myNumbers[x]);
+			}
+			
 		//End Loop for unsorted list
 		}
-		
-
 		//Move the data back to the array
+		for(int i = 0; i < sorted.size(); i ++)
+		{
+			myNumbers[i] = sorted.get(i);
+		}
 		//Print the contents of the array
-		System.out.println(sorted);
+		System.out.println(Arrays.toString(myNumbers));
     }
 
 
